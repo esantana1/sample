708dbbe13ae6232228293de20015a9a03d7ced0f
esantana1 <e_santana@post.massbay.edu>
2012-04-20 22:01:01 -0300

    Awsome

--- src/NewMain.java
+++ src/NewMain.java
@@ -5,7 +5,7 @@
 
 /**
  *
- * @author Eric S
+ * @author Eric San
  */
 public class NewMain {
 
@@ -13,6 +13,6 @@ public class NewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        // Awsome!!
+        }
 }
